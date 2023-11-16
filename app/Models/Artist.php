9a53823af8e7fe9d61ecc76cee2479390f476f96
panda0330 <superdev330@gmail.com>
2023-11-17 07:23:23 +0900

    artist change

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -26,30 +26,6 @@ class Artist extends Model
         'photo',
         'stock_value',
         'market_value',
-        'dividend_payments',
-        'country',
-        'change_stock',
-        'change_market',
-        'current_price',
-        'total_supply',
-        'circulating_supply',
-        'artist',
-        'music_fan_investor',
-        'sell_restrictions',
-        'market_maker_price',
-        'sell_limit'
 
-    ];
-   public function artistByUser($user_id){
-    $role= check_role();
-    if($role == 1){
-        $artist = Artist::where(['user_id'=>auth()->user()->id,'primary_artist'=>1])->first();
-       if(empty($artist)){
-        $artist = Artist::where('user_id',auth()->user()->id)->latest()->first();
-       }
-    }else{
-        $artist = Artist::where('user_id',$user_id)->first();
-    }
-       return $artist;
    }
 }
